688613f09f0561899c72b0b36bcdf91de71bec1d
Aries Sun <zx.sun@aftership.com>
2022-06-02 17:15:16 +0800

    update courier detect list

--- src/model/courier/courier_detect_list.ts
+++ src/model/courier/courier_detect_list.ts
@@ -1,5 +1,4 @@
 import { Courier } from './courier';
-import { Tracking } from '../tracking/tracking';
 
 /**
  * The response of couriers detect request
@@ -9,11 +8,6 @@ export interface CourierDetectList {
    * Total number of matched couriers
    */
   total: number;
-
-  /**
-   * Hash describes the tracking information.
-   */
-  tracking: Tracking;
 
   /**
    * A list of matched couriers based on tracking number format.
